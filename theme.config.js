7294828521c5b01516f31480c2e315fd1ce3cf7e
nokovacs <nkovacs@umich.edu>
2023-09-07 12:10:28 -0400

    intro and about page content

--- theme.config.js
+++ theme.config.js
@@ -4,7 +4,7 @@ export default {
   footer: (
     <footer>
       <small>
-        <time>{YEAR}</time> © Your Name.
+        <time>{YEAR}</time> © Nathan Kovacs.
         <a href="/feed.xml">RSS</a>
       </small>
       <style jsx>{`
